0dc3d551053bbd68a97a7e1f54ed73284c2284b8
Gwynfox <105600030+Gwynfox@users.noreply.github.com>
2022-09-11 10:54:06 +0300

    Добавьте файлы проекта.

---
+++ WriteWriteLine/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Transactions;
+
+namespace PracticalWork1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Write("Hello");
+            Console.Write(" World");
+            Console.Write("!!!");
+            Console.ReadKey();            
+        }
+    }
+}
